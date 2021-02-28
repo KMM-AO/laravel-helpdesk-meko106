c9f4a1b914d5ab47a6414b8cb221c2aadd8455af
meko106 <achiha@st.noorderpoort.nl>
2021-02-28 04:34:11 +0100

    3a

--- helpdesk/routes/web.php
+++ helpdesk/routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/greeting', function(){
+    return '<h1>Hello World</h1>';
 });
