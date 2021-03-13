e81c0d4370f3f4fe8b568893b8248b8e2646d0f8
meko106 <achiha@st.noorderpoort.nl>
2021-03-13 18:47:15 +0100

    6d

---
+++ helpdesk/database/seeders/RoleSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Role;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Role::createMany([
+        //     ['id'=>Role::BOSS, 'name'=>'baas'],
+        //     ['id'=>Role::EMPLOYEE, 'name'=>'medewerker'],
+        //     ['id'=>Role::APPLICANT, 'name'=>'sollicitant'],
+        //     ['id'=>Role::CUSTOMER, 'name'=>'klant']
+        // ]);
+        Role::factory()->createMany([ 
+            ['id'=>Role::BOSS, 'name'=>"baas"], 
+            ['id'=>Role::EMPLOYEE, 'name'=>"medewerker"],
+            ['id'=>Role::APPLICANT, 'name'=>"sollicitant"], 
+            ['id'=>Role::CUSTOMER, 'name'=>"klant"]
+        ]);
+    }
+}
