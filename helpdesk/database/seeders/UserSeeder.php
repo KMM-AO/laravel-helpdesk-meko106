cd0d3084570c6e47ad8f6a6ef486326b014fa352
meko106 <achiha@st.noorderpoort.nl>
2021-03-19 11:06:16 +0100

    6g

--- helpdesk/database/seeders/UserSeeder.php
+++ helpdesk/database/seeders/UserSeeder.php
@@ -22,7 +22,7 @@ class UserSeeder extends Seeder
         User::create([
             'name'=>'Meko',
             'email'=>'baas@helpdesk.nl',
-            'email_verified_at'=> now(),
+            'email_verified_at'=>now(),
             'password'=>Hash::make('Laravel2012'),
             'role_id'=>Role::BOSS
         ]);
