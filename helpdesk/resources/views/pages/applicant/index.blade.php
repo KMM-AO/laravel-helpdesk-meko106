368bc80792fe6ab9cce31301a2ccc09eae1f5968
meko106 <achiha@st.noorderpoort.nl>
2021-05-29 00:11:58 +0200

    11d

--- helpdesk/resources/views/pages/applicant/index.blade.php
+++ helpdesk/resources/views/pages/applicant/index.blade.php
@@ -10,12 +10,9 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    Hier komt een tabel met sollicitanten 
-                    <br>
 
                     <x-auth-session-status class="mb-4" :status="session('status')" />
 
-                    <br>
                     @if( $applicants->isEmpty() )
                     Currently no applications...
                     @else
