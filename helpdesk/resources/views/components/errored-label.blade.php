e41384f68d8b37fa77d163866d84a7d338245c39
meko106 <achiha@st.noorderpoort.nl>
2021-06-05 22:11:58 +0200

    13a

--- helpdesk/resources/views/components/errored-label.blade.php
+++ helpdesk/resources/views/components/errored-label.blade.php
@@ -1,6 +1,12 @@
 @props(['value' ,'field'])
 
-{{ $value }}
-@error($field)
-<span class="text-red-500">{{ $message }}</span>
-@enderror
+
+<label {{ $attributes->merge(['class' => 'block font-medium text-sm text-gray-700']) }}>
+    {{ $value }}
+
+    @error($field)
+    <span class="text-red-500">{{ $message }}</span>
+    @enderror
+
+</label>
+
